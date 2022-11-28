b79d50dd31097a1fb977669fc7f844ae0ae42772
crazyllama-admin <119366226+crazyllama-admin@users.noreply.github.com>
2022-11-28 17:33:09 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import plotly.express as px
+import pandas as pd
+​
+from dash import Dash, html, dcc, Input, Output
+​
+​
+app = Dash(__name__)
+​
+​
+df = pd.DataFrame({
+    'Campaign ID':  ['1844', '1846', '1911', '1845', '1852', '1928', '1854',],
+    'Chargebacks':  [470, 233, 151, 97, 95, 57, 50,],
+})
+​
+fig = px.bar(
+    df,
+    x='Chargebacks',
+    y='Campaign ID',
+    orientation='h',
+    barmode='group',
+    text_auto='<5i',
+)
+fig.update_layout(
+    title='Chargebacks by Campaign ID',
+    width=800,
+    height=600,
+    # marker={'color': '#ff0000', 'line': '#00ff00', 'width': 1.5},
+    plot_bgcolor='#0000ff',
+    paper_bgcolor='#0000ff',
+    yaxis={'autorange': 'reversed'},
+)
+fig.update_traces(
+    textposition='outside',
+)
+​
+app.layout = html.Div(children=[
+    dcc.Graph(id='chargebacks', figure=fig)
+])
+​
+​
+if __name__ == '__main__':
+    app.run_server(debug=True)
